c141950d77f9a13cfc9827b429ad2e8d010f0210
StrappedGlint13 <matiasbrax@gmail.com>
2021-10-11 12:09:44 +0300

    10.3 done

--- App.js
+++ App.js
@@ -1,23 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import RepositoryList from './src/components/RepositoryList';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Testi</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <RepositoryList />;
+};
 
-console.log('Testi');
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
