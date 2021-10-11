30b9458584995dd31e2c9d2fd46e55c8b4532eb1
StrappedGlint13 <matiasbrax@gmail.com>
2021-10-11 09:07:35 +0300

    initial commit

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Testi</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+console.log('Testi')
 
 const styles = StyleSheet.create({
   container: {
