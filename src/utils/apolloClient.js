2df29591959d6435e70d87eac3f7d83f9b865019
StrappedGlint13 <matiasbrax@gmail.com>
2021-10-25 10:05:44 +0300

    configurations for apollo_uri (10.12)

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,11 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
+
+const apollo_uri = Constants.manifest.extra.apollo;
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'http://192.168.10.47:5000/graphql',
+  uri: apollo_uri,
 });
 
 const createApolloClient = () => {
